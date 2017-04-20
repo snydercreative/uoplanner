a6860beac90737b3fa249fab7656f2d1dbefc223
snydercreative <me@michaelsnyder.me>
2017-04-19 20:06:06 -0400

    Tweeaked gulpfile and settings

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,28 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const stylish = require('jshint-stylish');
+const runSequence = require('run-sequence');
+const cssnano = require('gulp-cssnano');
 
-gulp.task('default', () => {
-	
+gulp.task('clean', () => {
+	return gulp.src('public')
+		.pipe(clean());
+});
+
+gulp.task('scss', () => {
+	return gulp.src('client/stylesheets/master.scss')
+		.pipe(sass())
+		.pipe(cssnano())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', ['clean'], (callback) => {
+	return runSequence(['scss'], callback);
+});
+
+gulp.task('watcher', () => {
+	gulp.watch('client/stylesheets/**/*.scss', ['default']);
+	gulp.watch('client/js/*.js', ['default']);
+	gulp.watch('client/js/utility/**/*.js', ['default']);
 });
