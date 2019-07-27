96a2abf629f77d88ac7c6db7bf68f707bc7362f1
Michael Snyder <snydercreative@gmail.com>
2019-07-27 15:01:56 -0400

    New version of Mongoose

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoUsername = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
 const connectionString = `mongodb://${mongoUsername}:${mongoPassword}@127.0.0.1:27017/uoplanner`;
 
-mongoose.connect(connectionString);
+mongoose.connect(connectionString, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({
     extended: true
