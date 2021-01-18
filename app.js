41b19048e379b28b96821e485f70adb287257662
Michael Snyder <me@michaelsnyder.me>
2021-01-18 16:21:32 -0500

    removed debug statement

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const mongoUsername = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
 const connectionString = `mongodb://${mongoUsername}:${mongoPassword}@127.0.0.1:27017/uoplanner`;
 
-console.log(connectionString)
-
 mongoose.connect(connectionString, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({
